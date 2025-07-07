2c9a8413c51b4e6edfcb152b56dd066d93ebfcb0
contem <contem.fe@gmail.com>
2025-07-07 17:58:07 +0300

    Integrate Element Plus and VeeValidate into main.js, enhancing the app's UI and form handling capabilities.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import './style.css'
+import { Field, Form, ErrorMessage } from 'vee-validate'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.use(ElementPlus)
+app.component('VeeForm', Form)
+app.component('VeeField', Field)
+app.component('ErrorMessage', ErrorMessage)
+app.mount('#app')
