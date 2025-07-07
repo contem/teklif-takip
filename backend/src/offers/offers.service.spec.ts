cc7f17c8a585bec89e77d0a4188e920f249cbc5e
contem <contem.fe@gmail.com>
2025-07-07 17:57:40 +0300

    Add mock repository providers for Offer and Customer entities in OffersService tests

--- backend/src/offers/offers.service.spec.ts
+++ backend/src/offers/offers.service.spec.ts
@@ -1,14 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OffersService } from './offers.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Offer } from './entities/offer.entity';
+import { Customer } from '../customers/entities/customer.entity';
 
 describe('OffersService', () => {
   let service: OffersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OffersService],
+      providers: [
+        OffersService,
+        { provide: getRepositoryToken(Offer), useValue: {} },
+        { provide: getRepositoryToken(Customer), useValue: {} },
+      ],
     }).compile();
-
     service = module.get<OffersService>(OffersService);
   });
 
